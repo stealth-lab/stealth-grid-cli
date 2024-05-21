5228b08a93cf0eade7b0c6d2dcb101551b42a249
rmaze <leo@maze.dev>
2024-05-21 15:20:12 -0300

    feature: (graphql.go docs)

--- pkg/graphql/graphql.go
+++ pkg/graphql/graphql.go
@@ -10,6 +10,7 @@ import (
 	"time"
 )
 
+// QueryVariables represents the variables for the GraphQL query.
 type QueryVariables struct {
 	StartTime   string `json:"startTime"`
 	EndTime     string `json:"endTime"`
@@ -17,13 +18,51 @@ type QueryVariables struct {
 	TitleIDs    string `json:"titleIds"`
 }
 
+// GraphQLRequest represents the structure of a GraphQL request.
 type GraphQLRequest struct {
 	Query     string         `json:"query"`
 	Variables QueryVariables `json:"variables"`
 }
 
-/*
- */
+// FetchData fetches data from a GraphQL API given a title ID and a time range.
+//
+// This function constructs a GraphQL query to fetch series data from the API
+// "https://api.grid.gg/central-data/graphql" based on the provided title ID and
+// time range. The data is retrieved using a POST request and is returned as a
+// map. If any error occurs during the process, it is returned.
+//
+// Parameters:
+//   - titleID: A string representing the ID of the title to query for. This is used
+//     to filter the series based on the specific title.
+//   - startTime: A time.Time object representing the start time of the query range.
+//     This is converted to RFC3339 format and used in the query to filter series
+//     that start on or after this time.
+//   - endTime: A time.Time object representing the end time of the query range.
+//     This is converted to RFC3339 format and used in the query to filter series
+//     that end on or before this time.
+//
+// Returns:
+//   - A map[string]interface{} containing the query results if the request is
+//     successful. The map includes information about the series such as total
+//     count, page info, and details about each series.
+//   - An error if the request fails at any point. Errors can occur during JSON
+//     marshalling of the request, creation of the HTTP request, sending the HTTP
+//     request, or decoding the JSON response.
+//
+// The function performs the following steps:
+//  1. Initializes a QueryVariables struct with the provided titleID, startTime,
+//     endTime, and an empty AfterCursor.
+//  2. Defines a GraphQL query string that requests series data filtered by the
+//     provided titleID and time range, ordering by start time and limiting the
+//     results to the first 50 series.
+//  3. Constructs a GraphQLRequest struct with the query string and variables.
+//  4. Marshals the GraphQLRequest struct into JSON format.
+//  5. Creates an HTTP POST request with the JSON payload to the specified API
+//     endpoint and sets the appropriate headers (Content-Type and x-api-key).
+//  6. Sends the HTTP request using an HTTP client and handles the response.
+//  7. Decodes the JSON response into a map[string]interface{}.
+//  8. Returns the decoded response map if successful, or an error if any step
+//     fails.
 func FetchData(titleID string, startTime, endTime time.Time) (map[string]interface{}, error) {
 	variables := QueryVariables{
 		StartTime:   startTime.Format(time.RFC3339),
@@ -98,14 +137,32 @@ func FetchData(titleID string, startTime, endTime time.Time) (map[string]interfa
 	return result, nil
 }
 
-/*
- */
+// DownloadJSON downloads a ZIP file for a given series ID from the specified API.
+//
+// This function constructs a URL to download a ZIP file related to the specified
+// series ID. It sends an HTTP GET request to the URL and handles the response,
+// saving the ZIP file locally. If any error occurs during the process, it logs
+// the error and terminates.
+//
+// Parameters:
+//   - serieID: A string representing the ID of the series to download the ZIP file for.
+//     This ID is used to construct the download URL.
+//
+// The function performs the following steps:
+//  1. Constructs the download URL using the provided series ID.
+//  2. Creates an HTTP GET request to the constructed URL.
+//  3. Sets the necessary headers (including the API key) for the request.
+//  4. Sends the request using an HTTP client and handles the response.
+//  5. Checks if the response status code is OK (200). If not, logs an error and terminates.
+//  6. Creates a file to save the downloaded ZIP content.
+//  7. Copies the content from the response body to the created file.
+//  8. Logs a success message if the file is saved successfully, or an error message if any step fails.
 func DownloadJSON(serieID string) {
 	url := fmt.Sprintf("https://api.grid.gg/file-download/events/grid/series/%s", serieID)
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Printf("Erro ao criar solicitação: %v", err)
+		fmt.Printf("Error creating request: %v\n", err)
 		return
 	}
 
@@ -114,28 +171,28 @@ func DownloadJSON(serieID string) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("Erro ao baixar o ZIP: %v", err)
+		fmt.Printf("Error downloading ZIP: %v\n", err)
 		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Erro: Código de status %d\n", resp.StatusCode)
+		fmt.Printf("Error: Status code %d\n", resp.StatusCode)
 		return
 	}
 
 	file, err := os.Create(fmt.Sprintf("%s.zip", serieID))
 	if err != nil {
-		fmt.Printf("Erro ao criar o arquivo: %v", err)
+		fmt.Printf("Error creating file: %v\n", err)
 		return
 	}
 	defer file.Close()
 
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
-		fmt.Printf("Erro ao salvar o ZIP no arquivo: %v", err)
+		fmt.Printf("Error saving ZIP to file: %v\n", err)
 		return
 	}
 
-	fmt.Printf("Arquivo ZIP para a série %s baixado com sucesso.\n", serieID)
+	fmt.Printf("ZIP file for series %s downloaded successfully.\n", serieID)
 }
