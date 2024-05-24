0092c50faeb174150a60bd0bec730d3dcd42abb3
Mateus Paulo <mat.paulo24@gmail.com>
2024-05-24 09:12:14 -0300

    add more doc

--- pkg/export/export.go
+++ pkg/export/export.go
@@ -9,6 +9,24 @@ import (
 	"github.com/charmbracelet/bubbles/table"
 )
 
+// ExportData exports the provided data to a CSV file named "games.csv".
+// The CSV file will include the headers "Start Time", "Serie ID", "Tournament", "Blue Team", and "Red Team".
+// Each row in the provided data will be written as a record in the CSV file.
+//
+// Parameters:
+//
+//	data: []table.Row - A slice of table rows containing the data to be exported. Each row is expected to have 5 elements:
+//	  - Start Time (string): The start time of the game
+//	  - Serie ID (string): The unique identifier of the series
+//	  - Tournament (string): The name of the tournament
+//	  - Blue Team (string): The name of the blue team
+//	  - Red Team (string): The name of the red team
+//
+// Behavior:
+//
+//	The function creates a new CSV file named "games.csv" in the current working directory. It writes the headers to the file,
+//	followed by each row of data. If an error occurs during file creation or writing, the function prints an error message
+//	to the console. Upon successful completion, a confirmation message is printed and the function pauses for 1 second.
 func ExportData(data []table.Row) {
 	fileName := "games.csv"
 	file, err := os.Create(fileName)
@@ -36,5 +54,4 @@ func ExportData(data []table.Row) {
 	}
 	fmt.Printf("Data successfully exported to %s\n", fileName)
 	time.Sleep(1 * time.Second)
-
 }
