4987e3daa3ba705e3c567629f6a19ddaacfed489
rmaze <leo@maze.dev>
2024-05-24 20:42:11 -0300

    feature: docs(export.go)

--- pkg/export/export.go
+++ pkg/export/export.go
@@ -12,23 +12,22 @@ import (
 )
 
 // ExportData exports the provided data to a CSV file selected by the user.
+//
 // The CSV file will include the headers "Start Time", "Serie ID", "Tournament", "Blue Team", and "Red Team".
 // Each row in the provided data will be written as a record in the CSV file.
 //
 // Parameters:
+//   - data: A slice of table rows containing the data to be exported. Each row is expected to have 5 elements:
+//   - Start Time (string): The start time of the game.
+//   - Serie ID (string): The unique identifier of the series.
+//   - Tournament (string): The name of the tournament.
+//   - Blue Team (string): The name of the blue team.
+//   - Red Team (string): The name of the red team.
 //
-//	data: []table.Row - A slice of table rows containing the data to be exported. Each row is expected to have 5 elements:
-//	  - Start Time (string): The start time of the game
-//	  - Serie ID (string): The unique identifier of the series
-//	  - Tournament (string): The name of the tournament
-//	  - Blue Team (string): The name of the blue team
-//	  - Red Team (string): The name of the red team
-//
-// Behavior:
-//
-//	The function opens a dialog for the user to select the save location and file name for the CSV file.
-//	It writes the headers to the file, followed by each row of data. If an error occurs during file creation or writing,
-//	the function prints an error message to the console. Upon successful completion, a confirmation message is printed and the function pauses for 1 second.
+// The function opens a dialog for the user to select the save location and file name for the CSV file.
+// It writes the headers to the file, followed by each row of data. If an error occurs during file creation or writing,
+// the function prints an error message to the console. Upon successful completion, a confirmation message is printed
+// and the function pauses for 1 second.
 func ExportData(data []table.Row) {
 	filePath, err := dialog.File().Title("Save CSV File").Save()
 	if err != nil || filePath == "" {
